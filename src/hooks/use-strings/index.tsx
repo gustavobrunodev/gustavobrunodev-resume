126d1fd39825ab2801afc09ef16bd70864ef9942
gustavobgt <gustavo_bgt97@hotmail.com>
2024-06-16 10:55:04 -0300

    refactor: fix imports

--- src/hooks/use-strings/index.tsx
+++ src/hooks/use-strings/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import type { Lang } from '~/types'
+import type { Lang } from '../../types'
 import strings from './strings.json'
 
 export interface StringsProviderProps {
@@ -7,6 +7,7 @@ export interface StringsProviderProps {
   lang: Lang
 }
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const StringsContext = React.createContext<ReturnType<typeof useContextValue>>()
 
--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
-import type { Theme } from '~/types'
+import type { Theme } from '../types'
 
 export interface ThemeProviderProps {
   children: React.ReactNode
   theme: Theme
 }
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const ThemeContext = React.createContext<Theme>()
 
